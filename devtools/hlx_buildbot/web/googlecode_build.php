eaa651c79084a2cc3a97396dc06ba2a1e203dd7a
Steven Sumichrast <packhead@gmail.com>
2010-06-29 19:47:02 -0500

    Added the two front-end files for the build bot scripts. build.php is manually called by developers to package up releases. googlecode_build.php is called by Google Code on a post-commit web hook.  It then calls the build_snapshot files for HLXCE.  It can also be used by other projects by specifying them in the case.

---
+++ devtools/hlx_buildbot/web/googlecode_build.php
@@ -0,0 +1,32 @@
+<?php
+
+# Google Code Post-Commit build wrapper
+#
+# This script is called by GoogleCode when a push is detected.
+
+// Check that we are getting a project parameter
+if (isset($_GET['project'])) 
+{
+	$project = $_GET['project'];
+}
+else
+{
+	die("This script can not be accessed directly.");
+}
+
+switch ($project) {
+	case "hlstatsxcommunity":
+		$data = file_get_contents("php://input");
+		$rev_data = json_decode($data);
+		$repository = $rev_data->repository_path;
+		if (!strpos($repository,"hlx-16")) 
+		{
+			$build_dev = 1;
+		} 
+		else
+		{
+			$build_stable = 1;
+		}
+		passthru("sudo -u hg /home/hg/hlx_buildbot/build_snapshot.sh $build_dev $build_stable");
+	break;
+}	
