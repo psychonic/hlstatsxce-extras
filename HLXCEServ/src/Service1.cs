778c76686f1e8b1d04c0fe23866b32319a7c3070
Nicholas Hastings <nshastings@gmail.com>
2011-01-11 09:58:57 -0500

    HLXCEServ: cleanup. no meaningful changes

--- HLXCEServ/src/Service1.cs
+++ HLXCEServ/src/Service1.cs
@@ -8,136 +8,135 @@ using System.ServiceProcess;
 
 namespace HLXCEServ
 {
-    public partial class HLXCEServ : ServiceBase
-    {
-        public HLXCEServ()
-        {
-            InitializeComponent();
-        }
- 
-        string g_strHLXCEPath;
-        string g_strPerlPath;
-        ushort g_iDaemonCount;
-        ushort g_iStartPort;
-        uint g_iMaxRetries;
+	public partial class HLXCEServ : ServiceBase
+	{
+		public HLXCEServ()
+		{
+			InitializeComponent();
+		}
+		
+		string g_strHLXCEPath;
+		string g_strPerlPath;
+		ushort g_iDaemonCount;
+		ushort g_iStartPort;
+		uint g_iMaxRetries;
 		ProcessPriorityClass g_priority;
 		bool g_bStartProxy;
-
-        List<Process> g_lprDaemons;
-        List<StreamWriter> g_lswLogFiles;
-        List<uint> g_liDaemonRetries;
-
+		
+		List<Process> g_lprDaemons;
+		List<StreamWriter> g_lswLogFiles;
+		List<uint> g_liDaemonRetries;
+		
 		SmtpClient g_Mailer;
 		ushort g_iNotifyLvl = 0;
-
-        protected override void OnStart(string[] args)
-        {
-            g_strHLXCEPath = ConfigurationManager.AppSettings["HLXCEPath"];
+		
+		protected override void OnStart(string[] args)
+		{
+			g_strHLXCEPath = ConfigurationManager.AppSettings["HLXCEPath"];
 			g_strPerlPath = ConfigurationManager.AppSettings["PerlPath"];
 			g_iDaemonCount = Convert.ToUInt16(ConfigurationManager.AppSettings["DaemonCount"]);
 			g_iStartPort = Convert.ToUInt16(ConfigurationManager.AppSettings["StartPort"]);
 			g_iMaxRetries = Convert.ToUInt32(ConfigurationManager.AppSettings["RetryCount"]);
-            g_lprDaemons = new List<Process>(g_iDaemonCount);
-            g_lswLogFiles = new List<StreamWriter>(g_iDaemonCount);
-            g_liDaemonRetries = new List<uint>(g_iDaemonCount);
+			g_lprDaemons = new List<Process>(g_iDaemonCount);
+			g_lswLogFiles = new List<StreamWriter>(g_iDaemonCount);
+			g_liDaemonRetries = new List<uint>(g_iDaemonCount);
 			g_priority = GetPriorityFromString(ConfigurationManager.AppSettings["Priority"]);
 			g_iNotifyLvl = Convert.ToUInt16(ConfigurationManager.AppSettings["EmailNotificationLvl"]);
 			string proxstart = ConfigurationManager.AppSettings["StartProxy"];
 			g_bStartProxy = (proxstart == "yes" || proxstart == "1");
-
+			
 			if (g_iNotifyLvl > 0)
 			{
 				SetupEmail();
 			}
-
+			
 			if (!File.Exists(g_strPerlPath + @"\perl.exe"))
 			{
 				DoError(String.Format("Failed to find {0:s}. Check your PerlPath setting in HLXCEServ.exe.config", g_strPerlPath + @"\perl.exe"));
 				this.Stop();
 			}
-
-            for (ushort i = 0; i < g_iDaemonCount; i++)
-            {
-                g_liDaemonRetries.Add(0);
-                g_lprDaemons.Add(null);
-                g_lswLogFiles.Add(null);
-            }
-
-            string strLogPath = g_strHLXCEPath + @"\logs";
-            if (!Directory.Exists(strLogPath))
-            {
-                EventLog.WriteEntry("\"" + g_strHLXCEPath + "\" does not exist; creating.", EventLogEntryType.Information);
-                try
-                {
-                    Directory.CreateDirectory(strLogPath);
-                }
-                catch (Exception ex)
-                {
-                    DoError(ex.Message);
-                    this.Stop();
-                }
-            }
-
-            for (ushort i = 0; i < g_iDaemonCount; i++)
-            {
-                SetupStartDaemon(i, false);
-            }
-        }
-
-        protected override void OnStop()
-        {
-            for (ushort i = 0; i < g_iDaemonCount; i++)
-            {
-                if (!g_lprDaemons[i].HasExited)
-                {
-                    g_lprDaemons[i].EnableRaisingEvents = false;
-                    //g_lprDaemons[i].Kill();
-                    g_lprDaemons[i].CloseMainWindow();
-                    g_lprDaemons[i].WaitForExit();
-                }
-                if (g_lswLogFiles[i] != null)
-                {
-                    g_lswLogFiles[i].Flush();
-                    g_lswLogFiles[i].Close();
-                }
-            }
-        }
-        void HLXCE_OutputReceived(object sender, DataReceivedEventArgs e)
-        {
-            for (ushort i = 0; i < g_iDaemonCount; i++)
-            {
-                if (sender.Equals(g_lprDaemons[i]))
-                {
-                    g_lswLogFiles[i].WriteLine(e.Data);
-                    if ((DateTime.Now.Second % 3) == 0)
-                    {
-                        g_lswLogFiles[i].Flush();
-                    }
-                    break;
-                }
-            }
-        }
+			
+			for (ushort i = 0; i < g_iDaemonCount; i++)
+			{
+				g_liDaemonRetries.Add(0);
+				g_lprDaemons.Add(null);
+				g_lswLogFiles.Add(null);
+			}
+			
+			string strLogPath = g_strHLXCEPath + @"\logs";
+			if (!Directory.Exists(strLogPath))
+			{
+				EventLog.WriteEntry("\"" + g_strHLXCEPath + "\" does not exist; creating.", EventLogEntryType.Information);
+				try
+				{
+					Directory.CreateDirectory(strLogPath);
+				}
+				catch (Exception ex)
+				{
+					DoError(ex.Message);
+					this.Stop();
+				}
+			}
+			
+			for (ushort i = 0; i < g_iDaemonCount; i++)
+			{
+				SetupStartDaemon(i, false);
+			}
+		}
+			
+		protected override void OnStop()
+		{
+			for (ushort i = 0; i < g_iDaemonCount; i++)
+			{
+				if (!g_lprDaemons[i].HasExited)
+				{
+					g_lprDaemons[i].EnableRaisingEvents = false;
+					g_lprDaemons[i].CloseMainWindow();
+					g_lprDaemons[i].WaitForExit();
+				}
+				if (g_lswLogFiles[i] != null)
+				{
+					g_lswLogFiles[i].Flush();
+					g_lswLogFiles[i].Close();
+				}
+			}
+		}
+		void HLXCE_OutputReceived(object sender, DataReceivedEventArgs e)
+		{
+			for (ushort i = 0; i < g_iDaemonCount; i++)
+			{
+				if (sender.Equals(g_lprDaemons[i]))
+				{
+					g_lswLogFiles[i].WriteLine(e.Data);
+					if ((DateTime.Now.Second % 3) == 0)
+					{
+						g_lswLogFiles[i].Flush();
+					}
+					break;
+				}
+			}
+		}
 		void HLXCE_ProxyExited(object sender, EventArgs e)
 		{
 			DoError("Proxy is no longer running; exiting.");
 			this.Stop();
 		}
-        void HLXCE_Exited(object sender, EventArgs e)
-        {
-            if (g_iDaemonCount <= 1)
-            {
+		void HLXCE_Exited(object sender, EventArgs e)
+		{
+			if (g_iDaemonCount <= 1)
+			{
 				DoError("HLXCE Daemon has exited unexpectedly");
 				this.Stop();
 				return;
 			}
-             
+			 
 			for (ushort i = 0; i < g_iDaemonCount; i++)
 			{
 				if (!sender.Equals(g_lprDaemons[i]))
 				{
 					continue;
 				}
-                     
+					 
 				if (++g_liDaemonRetries[i] > g_iMaxRetries)
 				{
 					int iRemainingDaemons = 0;
@@ -168,45 +167,45 @@ namespace HLXCEServ
 				break;
 			}
 		}
-
-        private void ExceptionFail(Exception ex)
-        {
-            DoError(ex.Message);
-            this.Stop();
-        }
-
-        private void SetupStartDaemon(ushort iDaemonId, bool bRelaunch)
-        {
-            ushort iPort = (ushort)(g_iStartPort + iDaemonId);
-            string strLogFilename;
-            string strPerlArgs;
-
-            if (g_iDaemonCount > 1)
-            {
-                strLogFilename = g_strHLXCEPath + @"\logs\" + iPort.ToString() + System.DateTime.Now.ToString("-yyyy-MM-dd-HH-mm-ss-ff") + ".log";
-                strPerlArgs = g_strHLXCEPath + @"\hlstats.pl --port=" + iPort.ToString();
-            }
-            else
-            {
-                strLogFilename = g_strHLXCEPath + @"\logs\" + System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss-ff") + ".log";
-                strPerlArgs = g_strHLXCEPath + @"\hlstats.pl";
-            }
-
-            if (bRelaunch)
-            {
-                g_lswLogFiles[iDaemonId].Flush();
-                g_lswLogFiles[iDaemonId].Close();
-            }
-
-            try
-            {
-                g_lswLogFiles[iDaemonId] = new StreamWriter(new FileStream(strLogFilename, System.IO.FileMode.OpenOrCreate));
-            }
-            catch (Exception ex)
-            {
-                ExceptionFail(ex);
-            }
-
+		
+		private void ExceptionFail(Exception ex)
+		{
+			DoError(ex.Message);
+			this.Stop();
+		}
+		
+		private void SetupStartDaemon(ushort iDaemonId, bool bRelaunch)
+		{
+			ushort iPort = (ushort)(g_iStartPort + iDaemonId);
+			string strLogFilename;
+			string strPerlArgs;
+			
+			if (g_iDaemonCount > 1)
+			{
+				strLogFilename = g_strHLXCEPath + @"\logs\" + iPort.ToString() + System.DateTime.Now.ToString("-yyyy-MM-dd-HH-mm-ss-ff") + ".log";
+				strPerlArgs = g_strHLXCEPath + @"\hlstats.pl --port=" + iPort.ToString();
+			}
+			else
+			{
+				strLogFilename = g_strHLXCEPath + @"\logs\" + System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss-ff") + ".log";
+				strPerlArgs = g_strHLXCEPath + @"\hlstats.pl";
+			}
+			
+			if (bRelaunch)
+			{
+				g_lswLogFiles[iDaemonId].Flush();
+				g_lswLogFiles[iDaemonId].Close();
+			}
+			
+			try
+			{
+				g_lswLogFiles[iDaemonId] = new StreamWriter(new FileStream(strLogFilename, System.IO.FileMode.OpenOrCreate));
+			}
+			catch (Exception ex)
+			{
+				ExceptionFail(ex);
+			}
+			
 			if (g_bStartProxy)
 			{
 				g_lprDaemons[iDaemonId] = new Process();
@@ -220,32 +219,32 @@ namespace HLXCEServ
 				g_lprDaemons[iDaemonId].Start();
 				g_lprDaemons[iDaemonId].PriorityClass = g_priority;
 			}
-
-            g_lprDaemons[iDaemonId] = new Process();
-            g_lprDaemons[iDaemonId].StartInfo.FileName = g_strPerlPath + @"\perl.exe";
-            g_lprDaemons[iDaemonId].StartInfo.Arguments = strPerlArgs;
-            g_lprDaemons[iDaemonId].StartInfo.WorkingDirectory = g_strHLXCEPath;
-            g_lprDaemons[iDaemonId].StartInfo.CreateNoWindow = true;
-            g_lprDaemons[iDaemonId].StartInfo.UseShellExecute = false;
-            g_lprDaemons[iDaemonId].StartInfo.RedirectStandardOutput = true;
-            g_lprDaemons[iDaemonId].StartInfo.RedirectStandardError = true;
+			
+			g_lprDaemons[iDaemonId] = new Process();
+			g_lprDaemons[iDaemonId].StartInfo.FileName = g_strPerlPath + @"\perl.exe";
+			g_lprDaemons[iDaemonId].StartInfo.Arguments = strPerlArgs;
+			g_lprDaemons[iDaemonId].StartInfo.WorkingDirectory = g_strHLXCEPath;
+			g_lprDaemons[iDaemonId].StartInfo.CreateNoWindow = true;
+			g_lprDaemons[iDaemonId].StartInfo.UseShellExecute = false;
+			g_lprDaemons[iDaemonId].StartInfo.RedirectStandardOutput = true;
+			g_lprDaemons[iDaemonId].StartInfo.RedirectStandardError = true;
 			g_lprDaemons[iDaemonId].ErrorDataReceived += new DataReceivedEventHandler(HLXCE_OutputReceived);
-            g_lprDaemons[iDaemonId].OutputDataReceived += new DataReceivedEventHandler(HLXCE_OutputReceived);
-            g_lprDaemons[iDaemonId].EnableRaisingEvents = true;
-            g_lprDaemons[iDaemonId].Exited += new EventHandler(HLXCE_Exited);
-            try
-            {
-                g_lprDaemons[iDaemonId].Start();
-                g_lprDaemons[iDaemonId].BeginOutputReadLine();
-                g_lprDaemons[iDaemonId].BeginErrorReadLine();
+			g_lprDaemons[iDaemonId].OutputDataReceived += new DataReceivedEventHandler(HLXCE_OutputReceived);
+			g_lprDaemons[iDaemonId].EnableRaisingEvents = true;
+			g_lprDaemons[iDaemonId].Exited += new EventHandler(HLXCE_Exited);
+			try
+			{
+				g_lprDaemons[iDaemonId].Start();
+				g_lprDaemons[iDaemonId].BeginOutputReadLine();
+				g_lprDaemons[iDaemonId].BeginErrorReadLine();
 				g_lprDaemons[iDaemonId].PriorityClass = g_priority;
-            }
-            catch (Exception ex)
-            {
-                ExceptionFail(ex);
-            }
-        }
-
+			}
+			catch (Exception ex)
+			{
+				ExceptionFail(ex);
+			}
+		}
+		
 		private void SetupEmail()
 		{
 			string username = ConfigurationManager.AppSettings["EmailUsername"];
@@ -256,7 +255,7 @@ namespace HLXCEServ
 				g_Mailer.Credentials = new System.Net.NetworkCredential(username, password);
 			}
 		}
-
+		
 		private void DoWarning(string message)
 		{
 			EventLog.WriteEntry(message, EventLogEntryType.Warning);
@@ -265,7 +264,7 @@ namespace HLXCEServ
 				g_Mailer.Send(new MailMessage(ConfigurationManager.AppSettings["EmailFrom"], ConfigurationManager.AppSettings["EmailTo"], "HLXCEServ Warning", "Warning from HLXCEServ:\n\n" + message + "\n"));
 			}
 		}
-
+		
 		private void DoError(string message)
 		{
 			EventLog.WriteEntry(message, EventLogEntryType.Error);
@@ -274,7 +273,7 @@ namespace HLXCEServ
 				g_Mailer.Send(new MailMessage(ConfigurationManager.AppSettings["EmailFrom"], ConfigurationManager.AppSettings["EmailTo"], "HLXCEServ Error", "Error from HLXCEServ:\n\n" + message + "\n"));
 			}
 		}
-
+		
 		private ProcessPriorityClass GetPriorityFromString(string sPriority)
 		{
 			sPriority = sPriority.ToLower();
@@ -293,5 +292,5 @@ namespace HLXCEServ
 			}
 			return ProcessPriorityClass.Normal;
 		}
-    }
+	}
 }
